0cee9bcad2cc50e920f1abea854e20561894640b
abdou fk <abdourrhamanefarkhsi@gmail.com>
2025-05-09 12:18:01 +0200

    ajout de la gestion de l'autorisation token swagger

--- src/main/java/com/chatop_back/api/config/SwaggerConfig.java
+++ src/main/java/com/chatop_back/api/config/SwaggerConfig.java
@@ -1,13 +1,15 @@
 package com.chatop_back.api.config;
 
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.servers.Server;
-
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
 
 @Configuration
 @OpenAPIDefinition(
@@ -15,9 +17,11 @@ import org.springframework.context.annotation.Configuration;
         title = "API Chatop",
         version = "v1",
         description = "Documentation de l'API Chatop pour interagir avec les locations et utilisateurs"
-    ), servers = {
-            @Server(description = "Local ENV", url = "http://localhost:3001/"),
-}
+    ),
+    servers = {
+        @Server(description = "Local ENV", url = "http://localhost:3001/")
+    },
+    security = @SecurityRequirement(name = "bearerAuth")  // Activation de l'authentification JWT
 )
 public class SwaggerConfig {
 
@@ -27,5 +31,17 @@ public class SwaggerConfig {
                 .group("public")
                 .pathsToMatch("/api/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .name("bearerAuth")))
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearerAuth"));
     }
 }
